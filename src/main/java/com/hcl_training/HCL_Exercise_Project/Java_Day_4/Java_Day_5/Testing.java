4b655be433c1d196ed4852286b597f15c339e2fc
ForestCurse12 <canalesjl31@gmail.com>
2021-06-30 19:13:10 -0500

    Added test file to check repo working status

---
+++ src/main/java/com/hcl_training/HCL_Exercise_Project/Java_Day_4/Java_Day_5/Testing.java
@@ -0,0 +1,10 @@
+package com.hcl_training.HCL_Exercise_Project.Java_Day_4.Java_Day_5;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing.");
+	}
+
+}
